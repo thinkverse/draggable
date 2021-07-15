7bc7f34bfa6c446c07e37e4e3d70e71d28a8c9ec
thinkverse <hallberg.kim@gmail.com>
2021-07-15 17:56:16 +0200

    Update experiment Handle link upload now with mocked responses

--- src/draggable.ts
+++ src/draggable.ts
@@ -1,29 +1,36 @@
-async function droppable(element: HTMLElement, callback: CallableFunction) {
-    const classes: string[] = ['border-8', 'border-dashed', 'border-blue-500'];
-
-    element.addEventListener('dragenter', (event: DragEvent) => {
+function intercept<T extends HTMLElement>(element: T, event: string, callback: CallableFunction|null = null): T {
+    element.addEventListener(event, async <T extends Event>(event: T) => {
         event.stopPropagation();
         event.preventDefault();
 
+        if (callback) {
+            await callback(element, event);
+        }
+    });
+
+    return element;
+}
+
+function tap<T extends HTMLElement>(element: T, callback: CallableFunction): T {
+    callback(element);
+
+    return element;
+}
+
+function droppable(element: HTMLElement, callback: CallableFunction) {
+    const classes: string[] = ['border-8', 'border-dashed', 'border-blue-500'];
+
+    intercept(element, 'dragenter', (element: HTMLElement) => {
         element.classList.add(...classes);
     });
 
-    element.addEventListener('dragleave', (event: DragEvent) => {
-        event.stopPropagation();
-        event.preventDefault();
-
+    intercept(element, 'dragleave', (element: HTMLElement) => {
         element.classList.remove(...classes);
     });
 
-    element.addEventListener('dragover', (event: DragEvent) => {
-        event.stopPropagation();
-        event.preventDefault();
-    });
+    intercept(element, 'dragover');
 
-    element.addEventListener('drop', (event: DragEvent) => {
-        event.stopPropagation();
-        event.preventDefault();
-
+    intercept(element, 'drop', (element: HTMLElement, event: DragEvent) => {
         if (event.dataTransfer?.files) {
             callback(element, event.dataTransfer);
         }
@@ -33,9 +40,37 @@ async function droppable(element: HTMLElement, callback: CallableFunction) {
 }
 
 ((document: Document) => {
-    droppable(document.body, async (element: HTMLElement , transfer: DataTransfer) => {
-        let form: HTMLFormElement|null = element.querySelector('form');
+    const form: HTMLFormElement|null = document.querySelector('form');
+    const input: HTMLInputElement|null = document.querySelector('input#link');
 
+    const paragraph: HTMLParagraphElement|null = document.querySelector('p');
+
+    if (! form || ! input || ! paragraph) {
+        alert('Element(s) are missing for this demo to work correctly.');
+
+        return;
+    }
+
+    intercept(form, 'submit', async (form: HTMLFormElement) => {
+        let response = await fetch(form.action, {
+            method: 'POST',
+            body: new FormData(form)
+        });
+
+        let result = await response.json();
+
+        input.setAttribute('value', '');
+
+        if (result.status !== 201) {
+            paragraph.innerText = result.message;
+        }
+
+        const url = new URL(window.location.toString());
+
+        paragraph.innerText = `${url.origin}${result.data.shortened_url}`;
+    });
+
+    droppable(document.body, async (element: HTMLElement , transfer: DataTransfer) => {
         const urls: Set<string> = new Set();
 
         for (let i = 0; i < transfer.items.length; i++) {
@@ -47,20 +82,8 @@ async function droppable(element: HTMLElement, callback: CallableFunction) {
         }
 
         if (urls.size > 0) {
-            if (! form) {
-                return;
-            }
-
-            tap(form, (form: HTMLFormElement) => {
-                form.children.item(0)?.children.namedItem('link')
-                    ?.setAttribute('value', urls.values().next().value);
-            }).submit();
+            tap(form, () => input.setAttribute('value', urls.values().next().value))
+                .dispatchEvent(new Event('submit'));
         }
     });
 })(document);
-
-function tap<T extends HTMLElement>(element: T, callback: CallableFunction): T {
-    callback(element);
-
-    return element;
-}
